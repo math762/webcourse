28d79b7210ef256f94f7bc23a354dfc87b19eb2f
math762 <matheus.liduvino762@gmail.com>
2023-06-12 11:48:23 -0300

    H2 database, test profile, JPA

--- src/main/java/com/sofitiuare/course/entities/User.java
+++ src/main/java/com/sofitiuare/course/entities/User.java
@@ -1,9 +1,17 @@
 package com.sofitiuare.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
+// A renomeação foi devido o nome do Objeto ser User cujo é um nome reservado;
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
